10cd6256b9beb3c11db2bdbd8a3321aa965dc64a
shraddha-123P <shraddha.pohankar93@gmail.com>
2026-04-27 17:42:56 +0530

    New changes

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let loginBtn = document.getElementById("loginbtn")
-let joinBtn = document.querySelector(".join")
+let joinBtn = document.getElementById("join")
 let participants = document.getElementById("participants")
-let controls = document.querySelector(".controls")
+let controls = document.getElementById("controls")
 let username = document.getElementById("username")
 let headerArea = document.getElementById("header")
 
@@ -9,35 +9,24 @@ let muteBtn = document.getElementById("muteBtn")
 let camBtn = document.getElementById("camBtn")
 let endCall = document.getElementById("endcall")
 
-
-
+// Login function
 loginBtn.onclick = function(){
 let name = username.value
-headerArea.innerHTML = name + ' <button id="logoutBtn">Logout</button>'
+headerArea.innerHTML = name + '<button id="logoutBtn">Logout</button>'
 joinBtn.style.display = "block"
 document.getElementById("login").style.display = "none"
 document.getElementById("logoutBtn").onclick = logout
 }
 
-// document.getElementById("logoutBtn").onclick = function(){
-// participants.style.display = "none"
-// controls.style.display = "none"
-// joinBtn.style.display = "none"
-// document.getElementById("login").style.display = "block"
-// username.value = ""
-// headerArea.innerHTML = '<button id="loginbtn">Login</button>'
-// document.getElementById("loginbtn").onclick = loginBtn.onclick
-// }
-
+// Join meeting
 joinBtn.onclick = function(){
 participants.style.display = "grid"
 controls.style.display = "block"
 }
 
 
-
+// Mute/Unmute
 muteBtn.onclick = function(){
-
 if(muteBtn.innerText == "Mute"){
 muteBtn.innerText = "Unmute"
 }else{
@@ -46,31 +35,25 @@ muteBtn.innerText = "Mute"
 
 }
 
-
+// Cam on/off
 camBtn.onclick = function(){
-
 if(camBtn.innerText == "Cam On"){
 camBtn.innerText = "Cam Off"
 }else{
 camBtn.innerText = "Cam On"
 }
-
 }
 
-
+// End call
 endCall.onclick = function(){
-
 participants.style.display = "none"
 controls.style.display = "none"
-
 }
 
-
+// logout
 function logout(){
-
 participants.style.display = "none"
 controls.style.display = "none"
-
 joinBtn.style.display = "none"
 
 document.getElementById("login").style.display = "block"
